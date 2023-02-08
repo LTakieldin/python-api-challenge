b0d483c6ad2981321e007c87168ff4e2e7e5a628
LTakieldin <louie.takieldin@gmail.com>
2023-02-07 23:20:32 -0500

    weatherpy

--- weatherpy/api_keys.py
+++ weatherpy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "35915d3e493b51340aee2a2882910f4a"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "5a6a884b48fe4fafb1bcdf3052e260e8"
